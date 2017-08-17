7f084131ec8e3615701645ba43884cc8e543c8f8
sgbowles <sgbowles@vt.edu>
2017-08-17 18:47:06 -0400

    basic widget running

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
-int main(){
-	return 0;
-}
+#include <QApplication>
+#include "window.hpp"
+
+int main(int argc, char *argv[]) {
+	QApplication app(argc, argv);
+
+	Window widget;
+	widget.show();
+	return app.exec();
+} 
