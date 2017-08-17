4a351074b16e513f2be2834fe686597bbcf6d0e0
sgbowles <sgbowles@vt.edu>
2017-08-17 18:50:53 -0400

    created year widget for display

--- year_widget.cpp
+++ year_widget.cpp
@@ -1,1 +1,19 @@
 //year_widget.cpp
+
+#include "year_widget.hpp"
+
+YearWidget::YearWidget(QWidget * parent) {
+	yearBox = new QDoubleSpinBox(this); //create box for contract years
+	yearBox->setDecimals(0); //need increments to be whole numbers
+	yearBox->setMinimum(0.0); //no negative numbers for years
+	yearBox->setMaximum(5.0); //5 year contract is the maximum
+
+	yearMessage = new QLabel("Enter the amount of years: ");
+
+	auto display = new QHBoxLayout;
+
+	display->addWidget(yearMessage);
+	display->addWidget(yearBox);
+
+	this->setLayout(display);
+}
--- year_widget.hpp
+++ year_widget.hpp
@@ -1,4 +1,23 @@
 #ifndef YEAR_WIDGET_HPP
 #define YEAR_WIDGET_HPP
 
+#include <QWidget>
+#include <QDoubleSpinBox>
+#include <QLayout>
+#include <QLabel>
+
+class QLineEdit;
+
+class YearWidget : public QWidget {
+	Q_OBJECT
+
+public:
+	YearWidget(QWidget * parent = nullptr);
+
+private:
+	QDoubleSpinBox *yearBox;
+	QLineEdit *display;
+	QLabel *yearMessage;
+};
+
 #endif
