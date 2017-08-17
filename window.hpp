2476a94f7e22a5635fcd3d181bea771ac10ef1c1
sgbowles <sgbowles@vt.edu>
2017-08-17 16:21:30 -0400

    skeleton of window.hpp

--- window.hpp
+++ window.hpp
@@ -1,1 +1,22 @@
-#window.hpp
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+#include <QWidget>
+
+class Window : public QWidget {
+Q_OBJECT
+
+public:
+	Window(QWidget * parent = nullptr);
+
+	Window(std::string filename, QWidget * parent = nullptr);
+
+private:
+	NameWidget *name;
+	SalaryWidget *sal;
+	YearWidger *year;
+
+};
+
+
+#endif
