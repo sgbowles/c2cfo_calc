c6ee3b5e7406559e88d2d8ab5cd0acb93039499b
sgbowles <sgbowles@vt.edu>
2017-08-17 18:50:18 -0400

    created salary widget for display

--- salary_widget.cpp
+++ salary_widget.cpp
@@ -1,1 +1,19 @@
 //salary_widget.cpp
+
+#include "salary_widget.hpp"
+
+SalaryWidget::SalaryWidget(QWidget * parent) {
+	salBox = new QDoubleSpinBox(this); //create box for salary
+	salBox->setDecimals(0); //need increments to be whole numbers
+	salBox->setMinimum(0.0); //no negative numbers for salary
+	salBox->setMaximum(100000.0); //this application should never need numbers higher than 100k
+	
+	salMessage = new QLabel("Enter the salary: ");
+
+	auto display = new QHBoxLayout;
+	
+	display->addWidget(salMessage);
+	display->addWidget(salBox);
+	
+	this->setLayout(display);
+}
--- salary_widget.hpp
+++ salary_widget.hpp
@@ -1,4 +1,23 @@
 #ifndef SALARY_WIDGET_HPP
 #define SALARY_WIDGET_HPP
 
+#include <QWidget>
+#include <QDoubleSpinBox>
+#include <QLayout>
+#include <QLabel>
+
+class QLineEdit;
+
+class SalaryWidget : public QWidget {
+Q_OBJECT
+
+public: 
+	SalaryWidget(QWidget * parent = nullptr);
+
+private:
+	QDoubleSpinBox *salBox;
+	QLineEdit *display;
+	QLabel *salMessage;
+};
+
 #endif
