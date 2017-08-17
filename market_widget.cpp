84526e991102d3c0cfc37ad871fff0a387ed785e
sgbowles <sgbowles@vt.edu>
2017-08-17 18:51:34 -0400

    created market widget for display

--- market_widget.cpp
+++ market_widget.cpp
@@ -1,1 +1,20 @@
 //market_widget.cpp
+
+#include "market_widget.hpp"
+
+MarketWidget::MarketWidget(QWidget * parent) {
+	marketBox = new QDoubleSpinBox(this); //create box for market value
+	//marketBox->isReadOnly();
+	marketBox->setDecimals(0); //no need for decimal places
+	marketBox->setMinimum(0.0); //no negative contract values
+	marketBox->setMaximum(100000.0); //this application should never need numbers higher than 100k
+
+	marketMessage = new QLabel("Market Vlaue: ");
+
+	auto display = new QHBoxLayout;
+
+	display->addWidget(marketMessage);
+	display->addWidget(marketBox);
+
+	this->setLayout(display);
+}
--- market_widget.hpp
+++ market_widget.hpp
@@ -1,4 +1,23 @@
 #ifndef MARKET_WIDGET_HPP
 #define MARKET_WIDGET_HPP
 
+#include <QWidget>
+#include <QDoubleSpinBox>
+#include <QLayout>
+#include <QLabel>
+
+class QLineEdit;
+
+class MarketWidget : public QWidget {
+	Q_OBJECT
+
+public:
+	MarketWidget(QWidget * parent = nullptr);
+
+private:
+	QDoubleSpinBox *marketBox;
+	QLineEdit *display;
+	QLabel *marketMessage;
+};
+
 #endif
